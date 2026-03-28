e32584eaada7eeeef04f1296a500fc292d62bf1e
eslam saied <eslamsaied051@gmail.com>
2026-03-28 17:41:13 +0200

    fix: deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/ip-address-tracker/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
-}))
+})
+
 
